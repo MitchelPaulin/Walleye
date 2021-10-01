4dc670a4c1292e545269df58dc86b49500f8f2f3
MitchelPaulin <mitchel0022@gmail.com>
2021-10-01 18:23:38 -0400

    Improved evaluation function

--- src/evaluation.rs
+++ src/evaluation.rs
@@ -2,176 +2,266 @@ pub use crate::board::*;
 pub use crate::board::{PieceColor::*, PieceKind::*};
 
 /*
-    Evaluation function based on https://www.chessprogramming.org/Simplified_Evaluation_Function
+    Evaluation function based on https://www.chessprogramming.org/PeSTO%27s_Evaluation_Function
 */
 
-const PAWN_WEIGHTS: [[i32; 8]; 8] = [
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [50, 50, 50, 50, 50, 50, 50, 50],
-    [10, 10, 20, 30, 30, 20, 10, 10],
-    [5, 5, 10, 25, 25, 10, 5, 5],
-    [0, 0, 0, 20, 20, 0, 0, 0],
-    [5, -5, -10, 0, 0, -10, -5, 5],
-    [5, 10, 10, -20, -20, 10, 10, 5],
-    [0, 0, 0, 0, 0, 0, 0, 0],
+#[rustfmt::skip]
+const MG_PAWN_TABLE: [[i32; 8]; 8] = [
+  [  0,   0,   0,   0,   0,   0,  0,   0],
+  [ 98, 134,  61,  95,  68, 126, 34, -11],
+  [ -6,   7,  26,  31,  65,  56, 25, -20],
+  [-14,  13,   6,  21,  23,  12, 17, -23],
+  [-27,  -2,  -5,  12,  17,   6, 10, -25],
+  [-26,  -4,  -4, -10,   3,   3, 33, -12],
+  [-35,  -1, -20, -23, -15,  24, 38, -22],
+  [  0,   0,   0,   0,   0,   0,  0,   0]
 ];
 
-const KNIGHT_WEIGHTS: [[i32; 8]; 8] = [
-    [-50, -40, -30, -30, -30, -30, -40, -50],
-    [-40, -20, 0, 0, 0, 0, -20, -40],
-    [-30, 0, 10, 15, 15, 10, 0, -30],
-    [-30, 5, 15, 20, 20, 15, 5, -30],
-    [-30, 0, 15, 20, 20, 15, 0, -30],
-    [-30, 5, 10, 15, 15, 10, 5, -30],
-    [-40, -20, 0, 5, 5, 0, -20, -40],
-    [-50, -40, -30, -30, -30, -30, -40, -50],
+#[rustfmt::skip]
+const EG_PAWN_TABLE: [[i32; 8]; 8] = [
+  [  0,   0,   0,   0,   0,   0,   0,   0],
+  [178, 173, 158, 134, 147, 132, 165, 187],
+  [ 94, 100,  85,  67,  56,  53,  82,  84],
+  [ 32,  24,  13,   5,  -2,   4,  17,  17],
+  [ 13,   9,  -3,  -7,  -7,  -8,   3,  -1],
+  [  4,   7,  -6,   1,   0,  -5,  -1,  -8],
+  [ 13,   8,   8,  10,  13,   0,   2,  -7],
+  [  0,   0,   0,   0,   0,   0,   0,   0],
 ];
 
-const BISHOP_WEIGHTS: [[i32; 8]; 8] = [
-    [-20, -10, -10, -10, -10, -10, -10, -20],
-    [-10, 0, 0, 0, 0, 0, 0, -10],
-    [-10, 0, 5, 10, 10, 5, 0, -10],
-    [-10, 5, 5, 10, 10, 5, 5, -10],
-    [-10, 0, 10, 10, 10, 10, 0, -10],
-    [-10, 10, 10, 10, 10, 10, 10, -10],
-    [-10, 5, 0, 0, 0, 0, 5, -10],
-    [-20, -10, -10, -10, -10, -10, -10, -20],
+#[rustfmt::skip]
+const MG_KNIGHT_TABLE: [[i32; 8]; 8] = [
+  [-167, -89, -34, -49,  61, -97, -15, -107],
+  [ -73, -41,  72,  36,  23,  62,   7,  -17],
+  [ -47,  60,  37,  65,  84, 129,  73,   44],
+  [  -9,  17,  19,  53,  37,  69,  18,   22],
+  [ -13,   4,  16,  13,  28,  19,  21,   -8],
+  [ -23,  -9,  12,  10,  19,  17,  25,  -16],
+  [ -29, -53, -12,  -3,  -1,  18, -14,  -19],
+  [-105, -21, -58, -33, -17, -28, -19,  -23],
 ];
 
-const ROOK_WEIGHTS: [[i32; 8]; 8] = [
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [5, 10, 10, 10, 10, 10, 10, 5],
-    [-5, 0, 0, 0, 0, 0, 0, -5],
-    [-5, 0, 0, 0, 0, 0, 0, -5],
-    [-5, 0, 0, 0, 0, 0, 0, -5],
-    [-5, 0, 0, 0, 0, 0, 0, -5],
-    [-5, 0, 0, 0, 0, 0, 0, -5],
-    [0, 0, 0, 5, 5, 0, 0, 0],
+#[rustfmt::skip]
+const EG_KNIGHT_TABLE: [[i32; 8]; 8] = [
+  [-58, -38, -13, -28, -31, -27, -63, -99],
+  [-25,  -8, -25,  -2,  -9, -25, -24, -52],
+  [-24, -20,  10,   9,  -1,  -9, -19, -41],
+  [-17,   3,  22,  22,  22,  11,   8, -18],
+  [-18,  -6,  16,  25,  16,  17,   4, -18],
+  [-23,  -3,  -1,  15,  10,  -3, -20, -22],
+  [-42, -20, -10,  -5,  -2, -20, -23, -44],
+  [-29, -51, -23, -15, -22, -18, -50, -64],
 ];
 
-const QUEEN_WEIGHTS: [[i32; 8]; 8] = [
-    [-20, -10, -10, -5, -5, -10, -10, -20],
-    [-10, 0, 0, 0, 0, 0, 0, -10],
-    [-10, 0, 5, 5, 5, 5, 0, -10],
-    [-5, 0, 5, 5, 5, 5, 0, -5],
-    [0, 0, 5, 5, 5, 5, 0, -5],
-    [-10, 5, 5, 5, 5, 5, 0, -10],
-    [-10, 0, 5, 0, 0, 0, 0, -10],
-    [-20, -10, -10, -5, -5, -10, -10, -20],
+#[rustfmt::skip]
+const MG_BISHOP_TABLE: [[i32; 8]; 8] = [
+  [-29,   4, -82, -37, -25, -42,   7,  -8],
+  [-26,  16, -18, -13,  30,  59,  18, -47],
+  [-16,  37,  43,  40,  35,  50,  37,  -2],
+  [ -4,   5,  19,  50,  37,  37,   7,  -2],
+  [ -6,  13,  13,  26,  34,  12,  10,   4],
+  [  0,  15,  15,  15,  14,  27,  18,  10],
+  [  4,  15,  16,   0,   7,  21,  33,   1],
+  [-33,  -3, -14, -21, -13, -12, -39, -21],
 ];
 
-const KING_WEIGHTS: [[i32; 8]; 8] = [
-    [-30, -40, -40, -50, -50, -40, -40, -30],
-    [-30, -40, -40, -50, -50, -40, -40, -30],
-    [-30, -40, -40, -50, -50, -40, -40, -30],
-    [-30, -40, -40, -50, -50, -40, -40, -30],
-    [-20, -30, -30, -40, -40, -30, -30, -20],
-    [-10, -20, -20, -20, -20, -20, -20, -10],
-    [20, 20, 0, 0, 0, 0, 20, 20],
-    [20, 30, 10, 0, 0, 10, 30, 20],
+#[rustfmt::skip]
+const EG_BISHOP_TABLE: [[i32; 8]; 8] = [
+  [-14, -21, -11,  -8, -7,  -9, -17, -24],
+  [ -8,  -4,   7, -12, -3, -13,  -4, -14],
+  [  2,  -8,   0,  -1, -2,   6,   0,   4],
+  [ -3,   9,  12,   9, 14,  10,   3,   2],
+  [ -6,   3,  13,  19,  7,  10,  -3,  -9],
+  [-12,  -3,   8,  10, 13,   3,  -7, -15],
+  [-14, -18,  -7,  -1,  4,  -9, -15, -27],
+  [-23,  -9, -23,  -5, -9, -16,  -5, -17],
 ];
 
-const KING_LATE_GAME: [[i32; 8]; 8] = [
-    [-50, -40, -30, -20, -20, -30, -40, -50],
-    [-30, -20, -10, 0, 0, -10, -20, -30],
-    [-30, -10, 20, 30, 30, 20, -10, -30],
-    [-30, -10, 30, 40, 40, 30, -10, -30],
-    [-30, -10, 30, 40, 40, 30, -10, -30],
-    [-30, -10, 20, 30, 30, 20, -10, -30],
-    [-30, -30, 0, 0, 0, 0, -30, -30],
-    [-50, -30, -30, -30, -30, -30, -30, -50],
+#[rustfmt::skip]
+const MG_ROOK_TABLE: [[i32; 8]; 8] = [
+  [ 32,  42,  32,  51, 63,  9,  31,  43],
+  [ 27,  32,  58,  62, 80, 67,  26,  44],
+  [ -5,  19,  26,  36, 17, 45,  61,  16],
+  [-24, -11,   7,  26, 24, 35,  -8, -20],
+  [-36, -26, -12,  -1,  9, -7,   6, -23],
+  [-45, -25, -16, -17,  3,  0,  -5, -33],
+  [-44, -16, -20,  -9, -1, 11,  -6, -71],
+  [-19, -13,   1,  17, 16,  7, -37, -26],
 ];
 
-// approximated at 2 kings, 2 bishops and 10 pawns
-const END_GAME_MATERIAL_VALUE: i32 = 41660;
+#[rustfmt::skip]
+const EG_ROOK_TABLE: [[i32; 8]; 8] = [
+  [13, 10, 18, 15, 12,  12,   8,   5],
+  [11, 13, 13, 11, -3,   3,   8,   3],
+  [ 7,  7,  7,  5,  4,  -3,  -5,  -3],
+  [ 4,  3, 13,  1,  2,   1,  -1,   2],
+  [ 3,  5,  8,  4, -5,  -6,  -8, -11],
+  [-4,  0, -5, -1, -7, -12,  -8, -16],
+  [-6, -6,  0,  2, -9,  -9, -11,  -3],
+  [-9,  2,  3, -1, -5, -13,   4, -20],
+];
+
+#[rustfmt::skip]
+const MG_QUEEN_TABLE: [[i32; 8]; 8] = [
+  [-28,   0,  29,  12,  59,  44,  43,  45],
+  [-24, -39,  -5,   1, -16,  57,  28,  54],
+  [-13, -17,   7,   8,  29,  56,  47,  57],
+  [-27, -27, -16, -16,  -1,  17,  -2,   1],
+  [ -9, -26,  -9, -10,  -2,  -4,   3,  -3],
+  [-14,   2, -11,  -2,  -5,   2,  14,   5],
+  [-35,  -8,  11,   2,   8,  15,  -3,   1],
+  [ -1, -18,  -9,  10, -15, -25, -31, -50],
+];
+
+#[rustfmt::skip]
+const EG_QUEEN_TABLE: [[i32; 8]; 8] = [
+  [ -9,  22,  22,  27,  27,  19,  10,  20],
+  [-17,  20,  32,  41,  58,  25,  30,   0],
+  [-20,   6,   9,  49,  47,  35,  19,   9],
+  [  3,  22,  24,  45,  57,  40,  57,  36],
+  [-18,  28,  19,  47,  31,  34,  39,  23],
+  [-16, -27,  15,   6,   9,  17,  10,   5],
+  [-22, -23, -30, -16, -16, -23, -36, -32],
+  [-33, -28, -22, -43,  -5, -32, -20, -41],
+];
+
+#[rustfmt::skip]
+const MG_KING_TABLE: [[i32; 8]; 8] = [
+  [-65,  23,  16, -15, -56, -34,   2,  13],
+  [ 29,  -1, -20,  -7,  -8,  -4, -38, -29],
+  [ -9,  24,   2, -16, -20,   6,  22, -22],
+  [-17, -20, -12, -27, -30, -25, -14, -36],
+  [-49,  -1, -27, -39, -46, -44, -33, -51],
+  [-14, -14, -22, -46, -44, -30, -15, -27],
+  [  1,   7,  -8, -64, -43, -16,   9,   8],
+  [-15,  36,  12, -54,   8, -28,  24,  14],
+];
+
+#[rustfmt::skip]
+const EG_KING_TABLE: [[i32; 8]; 8] = [
+  [-74, -35, -18, -18, -11,  15,   4, -17],
+  [-12,  17,  14,  17,  17,  38,  23,  11],
+  [ 10,  17,  23,  15,  20,  45,  44,  13],
+  [ -8,  22,  24,  27,  26,  33,  26,   3],
+  [-18,  -4,  21,  24,  27,  23,   9, -11],
+  [-19,  -3,  11,  21,  23,  16,   7,  -9],
+  [-27, -11,   4,  13,  14,   4,  -5, -17],
+  [-53, -34, -21, -11, -28, -14, -24, -43]
+];
+
+fn mg_table(kind: PieceKind) -> &'static [[i32; 8]; 8] {
+    match kind {
+        Pawn => &MG_PAWN_TABLE,
+        Bishop => &MG_BISHOP_TABLE,
+        Knight => &MG_KNIGHT_TABLE,
+        Rook => &MG_ROOK_TABLE,
+        King => &MG_KING_TABLE,
+        Queen => &MG_QUEEN_TABLE,
+    }
+}
+
+fn eg_table(kind: PieceKind) -> &'static [[i32; 8]; 8] {
+    match kind {
+        Pawn => &EG_PAWN_TABLE,
+        Bishop => &EG_BISHOP_TABLE,
+        Knight => &EG_KNIGHT_TABLE,
+        Rook => &EG_ROOK_TABLE,
+        King => &EG_KING_TABLE,
+        Queen => &EG_QUEEN_TABLE,
+    }
+}
+
+fn mg_piece_val(kind: PieceKind) -> i32 {
+    match kind {
+        Pawn => 82,
+        Knight => 337,
+        Bishop => 365,
+        Rook => 477,
+        Queen => 1025,
+        King => 0,
+    }
+}
+
+fn eg_piece_val(kind: PieceKind) -> i32 {
+    match kind {
+        Pawn => 94,
+        Knight => 281,
+        Bishop => 297,
+        Rook => 512,
+        Queen => 936,
+        King => 0,
+    }
+}
+
+fn game_phase_val(kind: PieceKind) -> i32 {
+    match kind {
+        Pawn => 0,
+        Knight => 1,
+        Bishop => 1,
+        Rook => 2,
+        Queen => 4,
+        King => 0,
+    }
+}
 
 /*
     Return how good a position is from the perspective of the current player
 */
 pub fn get_evaluation(board: &BoardState) -> i32 {
-    let mut evaluation = 0;
-    let mut total_piece_value = 0;
+    let mut white_mg = 0;
+    let mut black_mg = 0;
+    let mut white_eg = 0;
+    let mut black_eg = 0;
+    let mut game_phase = 0;
+
     for row in BOARD_START..BOARD_END {
         for col in BOARD_START..BOARD_END {
             if let Square::Full(Piece { color, kind }) = board.board[row][col] {
-                total_piece_value += kind.value();
-                if color == board.to_move {
-                    evaluation += get_pos_evaluation(row, col, color, kind) + kind.value();
-                } else {
-                    evaluation -= get_pos_evaluation(row, col, color, kind) + kind.value();
-                }
+                game_phase += game_phase_val(kind);
+                match color {
+                    White => {
+                        white_mg += mg_table(kind)[row - BOARD_START][col - BOARD_START]
+                            + mg_piece_val(kind);
+                        white_eg += eg_table(kind)[row - BOARD_START][col - BOARD_START]
+                            + eg_piece_val(kind);
+                    }
+                    Black => {
+                        black_mg += mg_table(kind)[9 - row][col - BOARD_START] + mg_piece_val(kind);
+                        black_eg += eg_table(kind)[9 - row][col - BOARD_START] + eg_piece_val(kind);
+                    }
+                };
             }
         }
     }
 
-    // an approximation of when the end game has started
-    let is_end_game = total_piece_value <= END_GAME_MATERIAL_VALUE;
-    let white_king_eval = get_pos_evaluation_king_white(&board, is_end_game);
-    let black_king_eval = get_pos_evaluation_king_black(&board, is_end_game);
-    evaluation += match board.to_move {
-        White => white_king_eval - black_king_eval,
-        Black => black_king_eval - white_king_eval,
-    };
-
-    evaluation
-}
-
-/*
-    Get the pos evaluation for every piece excluding the King
-*/
-fn get_pos_evaluation(row: usize, col: usize, color: PieceColor, kind: PieceKind) -> i32 {
-    let col = col - BOARD_START;
-    let row = match color {
-        White => row - BOARD_START,
-        Black => 9 - row,
-    };
-
-    match kind {
-        Pawn => PAWN_WEIGHTS[row][col],
-        Rook => ROOK_WEIGHTS[row][col],
-        Bishop => BISHOP_WEIGHTS[row][col],
-        Knight => KNIGHT_WEIGHTS[row][col],
-        Queen => QUEEN_WEIGHTS[row][col],
-        _ => 0,
+    let mg_score;
+    let eg_score;
+    if board.to_move == White {
+        mg_score = white_mg - black_mg;
+        eg_score = white_eg - black_eg;
+    } else {
+        mg_score = black_mg - white_mg;
+        eg_score = black_eg - white_eg;
     }
-}
 
-/*
-    Get the pos evaluation for the kings
-    We make this a separate function in order to avoid looping over the board more than once
-*/
+    let mut mg_phase = game_phase;
 
-fn get_pos_evaluation_king_white(board: &BoardState, is_end_game: bool) -> i32 {
-    if is_end_game {
-        return KING_LATE_GAME[board.white_king_location.0 - BOARD_START]
-            [board.white_king_location.1 - BOARD_START];
+    /* in case of early promotion */
+    if mg_phase > 24 {
+        mg_phase = 24;
     }
-    KING_WEIGHTS[board.white_king_location.0 - BOARD_START]
-        [board.white_king_location.1 - BOARD_START]
+    let eg_phase = 24 - mg_phase;
+    (mg_score * mg_phase + eg_score * eg_phase) / 24
 }
 
-fn get_pos_evaluation_king_black(board: &BoardState, is_end_game: bool) -> i32 {
-    if is_end_game {
-        return KING_LATE_GAME[9 - board.black_king_location.0]
-            [board.black_king_location.1 - BOARD_START];
-    }
-    KING_WEIGHTS[9 - board.black_king_location.0][board.black_king_location.1 - BOARD_START]
-}
 
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
-    fn position_evaluation() {
+    fn position_evaluation_equal() {
         let b = BoardState::from_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
             .unwrap();
         assert_eq!(get_evaluation(&b), 0);
-    }
-
-    #[test]
-    fn position_evaluation2() {
-        let b = BoardState::from_fen("rnbqkbnr/1ppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
-            .unwrap();
-        assert_eq!(get_evaluation(&b), 105);
     }
 }
