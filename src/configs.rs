22d53173cf032354543a2bbd447f801380ef7366
MitchelPaulin <mitchel0022@gmail.com>
2021-10-20 19:59:40 -0400

    Bumped version to 1.5

--- src/configs.rs
+++ src/configs.rs
@@ -1,6 +1,6 @@
 // During testing I found a depth of 6 to perform best on the optimized build, recommend depth 4 on debug build
 pub const DEFAULT_DEPTH: &str = "6";
 pub const MAX_DEPTH: u8 = 100; // don't let the engine search deeper than this
-pub const VERSION: &str = "1.4.0";
+pub const VERSION: &str = "1.5.0";
 pub const AUTHOR: &str = "Mitchel Paulin <mitchel0022@gmail.com>";
 pub const ENGINE_NAME: &str = "Walleye";
